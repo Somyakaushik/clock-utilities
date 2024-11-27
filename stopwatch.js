8b80ebb1d1ea764ce8b434a04f01dd430dbc3af4
Somyakaushik <129931581+Somyakaushik@users.noreply.github.com>
2024-11-27 22:52:55 +0530

    Update stopwatch.js

--- stopwatch.js
+++ stopwatch.js
@@ -1,35 +1,42 @@
 let seconds = 0;
 let minutes = 0;
+let milliseconds = 0;
 let interval;
 let isRunning = false;
 
 function updateDisplay() {
     const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
     const formattedSeconds = seconds < 10 ? '0' + seconds : seconds;
+    const formattedMilliseconds = milliseconds < 10 ? '00' + milliseconds : milliseconds < 100 ? '0' + milliseconds : milliseconds;
 
     const stopwatch = document.getElementsByClassName("stopwatch")[0];
-    stopwatch.innerHTML = `<p>${formattedMinutes}:${formattedSeconds}</p>`;
+    stopwatch.innerHTML = `<p>${formattedMinutes}:${formattedSeconds}:${formattedMilliseconds}</p>`;
 }
 
 function incrementTime() {
-    seconds++;
-    if (seconds === 60) {
-        seconds = 0;
-        minutes++;
+    milliseconds += 10; 
+
+    if (milliseconds >= 1000) {
+        milliseconds = 0;
+        seconds++;
+        if (seconds === 60) {
+            seconds = 0;
+            minutes++;
+        }
     }
     updateDisplay();
 }
 
 function start() {
     if (!isRunning) {
-        interval = setInterval(incrementTime, 1000);
+        interval = setInterval(incrementTime, 10); 
         isRunning = true;
     }
 }
 
 function stop() {
-    clearInterval(interval); 
-    isRunning = false; 
+    clearInterval(interval);
+    isRunning = false;
 }
 
 function reset() {
@@ -37,8 +44,10 @@ function reset() {
     isRunning = false;
     seconds = 0;
     minutes = 0;
-    updateDisplay();  
+    milliseconds = 0;
+    updateDisplay();
 }
 
 document.getElementsByClassName("start")[0].addEventListener("click", start);
 document.getElementsByClassName("stop")[0].addEventListener("click", stop);
+document.getElementsByClassName("reset")[0].addEventListener("click", reset);
